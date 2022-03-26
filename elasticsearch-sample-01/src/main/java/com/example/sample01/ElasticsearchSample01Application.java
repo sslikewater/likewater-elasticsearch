a71fabe2edd5181c7b18e4df08253b03f8403cb5
1147113180@qq.com <1147113180@qq.com>
2022-03-26 09:20:54 +0800

    init

---
+++ elasticsearch-sample-01/src/main/java/com/example/sample01/ElasticsearchSample01Application.java
@@ -0,0 +1,13 @@
+package com.example.sample01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchSample01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchSample01Application.class, args);
+    }
+
+}
---
+++ elasticsearch-sample-01/src/test/java/com/example/sample01/ElasticsearchSample01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchSample01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
